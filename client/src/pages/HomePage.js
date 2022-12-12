8d2c01176bf5753d435371b93c67653c0fa2665f
margoshkari <68592214+margoshkari@users.noreply.github.com>
2022-12-12 17:44:42 +0200

    home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,33 @@
-import '../css/Home.css'
-import logo from "../source/person-outline.svg"
+import "../css/Home.css";
+import person from "../source/person-outline.svg";
 import { Link } from "react-router-dom";
 
-function HomeApp(){
-    
-    return (
-        <div className="home-page">
-            <h1>Home</h1>
+function HomeApp() {
+  return (
+    <div className="home-page">
+      <div className="nav-panel">
+        <div className="nav-panf">
+          <h4>Автомобілі з пробігом</h4>
+            <Link id="account-btn" to="/login">
+                <div id="person-img-acc">
+                    <img src={person} alt="not found"></img>
+                    Увійти
+                </div>
+              </Link>
         </div>
-    )
+        <div className="line"></div>
+        <div className="nav-pan-sec">
+            <div className="nav-menu">
+                <span>Продати</span>
+                <span>Купити</span>
+                <span>Послуги</span>
+                <span>Про Компанію</span>
+                <span>Контакти</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeApp;
